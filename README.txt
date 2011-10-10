4e5ec7bbc515e25e809f0ea719c48bcca871e406
David Tang <dtang85@gmail.com>
2011-10-09 17:52:17 -0700

    fixed unsubscribe bug. PS method arguments slightly changed, so read documentation before using if you used the last version

--- README.txt
+++ README.txt
@@ -1,17 +1,17 @@
-Check out the demo page for examples on how to use the methods of the PS object.
+Check out the demo pages for examples on how to use the methods of the PS object.
 
 ====Subscribe======
-Method: subscribe(topic, function)
-Example: var subscription1 = PS.subscribe('newSearch', function() { /*your code goes here*/ });
-Return Value: The subscribe method returns a subscription identifier that can be used for unsubscribing the function from this topic
+Method: subscribe(topic, subName, function)
+Example: PS.subscribe('search', 'handleResults', function() { /*your code goes here*/ });
 
 ====Unsubscribe====
-Method: unsubscribe(topic, index), index refers to the subscription ID returned from the subscribe method
-Example: PS.unsubscribe('newSearch', subsciption1);
+Method: unsubscribe(topic, subName)
+subName - a unique string identifier for a subscription
+Example: PS.unsubscribe('search', 'handleResults');
 
 ====Publish========
 Method: publish(topic [,arg]), 
 Arguments: arg is an optional array of data that you would like to go along with a publication
-Example: PS.publish('newSearch', [searchPhrase, timeOfSearch]); where searchPhrase and timeOfSearch represent variables
+Example: PS.publish('search', [searchPhrase, timeOfSearch]); where searchPhrase and timeOfSearch represent variables
 
 
--- pubsub.js
+++ pubsub.js
@@ -1,54 +1,46 @@
 var PS = {
-	//topicList is an array of topics. Each topic contains an array of functions
-	topicList: [],
+	 
+	/* topicList is an object containing topics. 
+	 * Each topic is an associative array, where the key is the subscription name and the value is the function
+	 */
+	topicList: {},
 	
-	//subscribe a function to a particular topic
-	subscribe: function (topic, cb) {
-		if (typeof topic !== 'string'){
-			throw new Error('Topic must be a string');
-			return;
+	subscribe: function (topic, subName, cb) {
+		if (typeof topic !== 'string' && typeof subName !== 'string' && typeof cb !== 'function'){
+			throw new Error('Arguments of the wrong type.');
 		}
-	
-		if (typeof cb !== 'function') {
-			throw new Error('Callback must be a function')
-			return;
-		}
-	
+		
 		//if the topic doesn't exist, create the topic
 		if ( !(this.topicList[topic]) ) {
 			this.topicList[topic] = [];
 		}
-		
-		//push the cb function to the topic i.e. 'subscribe to that topic'
-		this.topicList[topic].push(cb);
-		
-		//return current index so that it can be used for unsubscribing
-		return (this.topicList[topic].length - 1);
+
+		//add the cb function to the topic under the key subName
+		//if the subscription name already exists, the new function will override the old one
+		this.topicList[topic][subName] = cb;
 	},
 	
-	/*
-	* unsubscribe a function from a topic. index argument refers to the position  
-	* for where the fn you want to remove resides for a specified topic.
-	* topicList['topic1'] = [fn1, fn2, fn3 ...]
-	*/
-	unsubscribe: function (topic, index) {
-		this.topicList[topic].splice(index, 1);
+	unsubscribe: function (topic, subName) {
+		if(typeof topic !== 'string' && typeof subName !== 'string') {
+			throw new Error('Arguments of the wrong type.')
+		}	
+		delete this.topicList[topic][subName];
 	},
 	
-	//publish a topic
 	publish: function (topic) {
-		var len = null, arg = [];
+		var arg = [];
 		
-		//check if the topic exists. if it doesnt, return from the function
-		if (this.topicList[topic] === undefined) {
-			throw new Error('No subscriptions to topic: '+topic);
-			return;
+		if (typeof topic !== 'string'){
+			throw new Error('Topic must be a string');
 		}
 		
-		//get the number of subscriptions for this topic
-		len = this.topicList[topic].length;
+		//check if the topic exists. a topic exists if there are any subscriptions to it
+		//if it doesnt exist, dont allow the user to publish this topic
+		if (this.topicList[topic] === undefined) {
+			throw new Error('No subscriptions to topic: '+topic);
+		}
 		
-		//any arguments passed the subscribed fn's will need to be an array
+		//any arguments passed to the subscribed fn's will need to be an array
 		if ( (arguments[1] !== undefined) && (arguments[1] instanceof Array) ) {
 			arg = arguments[1];
 		}
@@ -57,9 +49,10 @@ var PS = {
 		 * iterate over the subscriptions for this topic and invoke each cb function
 		 * passing it any arguments from the arguments array at position 1
 		 */
-		for (var i=0; i<len; i++) {
-			//invoke fn for specified topic, passing it an array of arguments
-			this.topicList[topic][i](arg);
+		for(var key in this.topicList[topic]) {
+			if(this.topicList.hasOwnProperty(topic)) {
+				this.topicList[topic][key](arg);
+			}
 		}
 	}
 };
