1876e3f6031d1b0d493204351efb75620d969d8a
David <dtang85@gmail.com>
2012-09-14 18:20:40 -0700

    updated publish test using Jasmine spies

--- spec/PSSpec.js
+++ spec/PSSpec.js
@@ -2,7 +2,7 @@ describe('PS', function() {
     var ps, sub1, unsub1;
 
 
-    describe('subscriptions', function() {
+    describe('subscribe()', function() {
         beforeEach(function() {
             ps = new PS();
 
@@ -12,19 +12,17 @@ describe('PS', function() {
         });
 
         describe('topicList', function() {
-            it('should have 1 key with 1 subscription function in the cb property', function() {
+            it('should have 1 key with an object value containing the subscription function in the cb property', function() {
                 expect(typeof ps.topicList['test-topic'][1].cb).toEqual('function');
             });
         });
 
-        describe('subscribe', function() {
-            it('should return a subscription identifier string', function() {
-                expect(sub1).toBeDefined();
-            });
+        it('should return a subscription identifier string', function() {
+            expect(sub1).toBeDefined();
         });
     });
 
-    describe('unsubscriptions', function() {
+    describe("unsubscribe('test-topic')", function() {
         beforeEach(function() {
             ps = new PS();
 
@@ -41,22 +39,44 @@ describe('PS', function() {
             });
         });
 
-        describe('unsubscribe', function() {
-            it('should return true for a successful unsubscription', function() {
-                expect(unsub1).toBe(true);
-            });
+        it('should return true for a successful unsubscription', function() {
+            expect(unsub1).toBe(true);
+        });
 
-            it('should return false for an unsuccessful unsubscription', function() {
-                expect(ps.unsubscribe('test-topic', 99)).toBe(false);
-            });
+        it('should return false for an unsuccessful unsubscription', function() {
+            expect(ps.unsubscribe('test-topic', 99)).toBe(false);
         });
     });
 
 
-    // describe('publications', function() {
-    //     beforeEach(function() {
-    //         ps = new PS();
-    //     });
-    // });
+    describe("publish('test-topic')", function() {
+        beforeEach(function() {
+            ps = new PS();
+
+            ps.subscribe('test-topic', function() {
+                console.log('some function');
+            });
+
+            ps.subscribe('test-topic', function() {
+                console.log('some function');
+            });
+
+            ps.subscribe('test-topic', function() {
+                console.log('some function');
+            }, { name: 'David', age: 26});
+        });
+
+        it("should invoke the 3 callback functions registered to test-topic", function() {
+            var spy1 = spyOn(ps.topicList['test-topic'][1], 'cb');
+            var spy2 = spyOn(ps.topicList['test-topic'][2], 'cb');
+            var spy3 = spyOn(ps.topicList['test-topic'][3], 'cb');
+
+            ps.publish('test-topic', 'data-here');
+
+            expect(spy1).toHaveBeenCalledWith('data-here');
+            expect(spy2).toHaveBeenCalledWith('data-here');
+            //expect(spy3).
+        });
+    });
 
 });
